4d2684eb58eb1263a9d01adca9e566e2ca827188
Yosuke Onoue <onoue@likr-lab.com>
2020-03-14 17:19:24 +0900

    add pwa settings

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import '@ionic/core/css/core.css'
 import '@ionic/core/css/ionic.bundle.css'
 import './index.css'
+import * as serviceWorker from './serviceWorker'
 
 import('./app')
+
+serviceWorker.register()
